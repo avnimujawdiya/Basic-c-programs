8c19bd6678b0f7767b88da5f77509941d8140e99
Avni mujawdiya <avnimujawdiya008@gmail.com>
2025-11-02 10:52:39 +0530

    Add basic calculator functionality in calculator.c
    Implement basic calculator operations: addition, subtraction, multiplication, and division.

---
+++ calculator.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{ 
+    int a,b;
+    int c, d, e, f;     
+    scanf("%d %d", &a, &b);
+    
+    c = a+b;
+    d = a-b;
+    e = a/b;
+    f = a*b;
+    
+    printf("value is %d\n",c);
+    printf("value is %d\n",d);
+    printf("value is %d\n",e);
+    printf("value is %d\n",f);
+    
+    return 0;
+}
