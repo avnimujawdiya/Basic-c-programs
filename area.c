980e70375281132a9414b3214fea6f000ab77234
Avni mujawdiya <avnimujawdiya008@gmail.com>
2025-11-02 11:02:06 +0530

    Add program to calculate area of a circle
    This program calculates the area of a circle based on user input for the radius.

---
+++ area.c
@@ -0,0 +1,14 @@
+// write a program to calculate area of circle
+
+#include<stdio.h>
+
+int main()
+{
+    float q ;
+    int r;
+    scanf("%d",&r);
+    q = 3.14*r*r;
+    printf("%f",q);
+    
+    return 0;
+}
